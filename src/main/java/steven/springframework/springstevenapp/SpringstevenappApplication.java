c085c0166e60e220ee08d03d3fc84114e75c9b5a
steven huang <steven.huang@highlimitstudio.com>
2023-07-21 21:45:02 +1000

    entity

---
+++ src/main/java/steven/springframework/springstevenapp/SpringstevenappApplication.java
@@ -0,0 +1,13 @@
+package steven.springframework.springstevenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstevenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstevenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/steven/springframework/springstevenapp/SpringstevenappApplicationTests.java
@@ -0,0 +1,13 @@
+package steven.springframework.springstevenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstevenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
